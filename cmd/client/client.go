d8e4d4d043775f95938bf2dea6c31a56f09fbe9c
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-06-25 22:18:12 -0300

    server and client stream

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -20,7 +20,8 @@ func main() {
 
 	client := pb.NewUserServiceClient(conn)
 	//AddUserStream(client)
-	AddUsers(client)
+	//AddUsers(client)
+	AddUserStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -97,4 +98,71 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUserStreamBoth(client pb.UserServiceClient) {
+	stream, err := client.AddUserStreamBoth(context.Background())
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "0",
+			Name:  "John Doe",
+			Email: "john@doe.com",
+		},
+		&pb.User{
+			Id:    "1",
+			Name:  "Jane Doe",
+			Email: "jane@doe.com",
+		},
+		&pb.User{
+			Id:    "2",
+			Name:  "Jack Doe",
+			Email: "jack@doe.com",
+		},
+		&pb.User{
+			Id:    "3",
+			Name:  "Paul Doe",
+			Email: "paul@doe.com",
+		},
+		&pb.User{
+			Id:    "4",
+			Name:  "Robert Doe",
+			Email: "robert@doe.com",
+		},
+		&pb.User{
+			Id:    "5",
+			Name:  "Maria Doe",
+			Email: "maria@doe.com",
+		},
+	}
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending Name: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	wait := make(chan int)
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Could not read stream: %v", err)
+			}
+			fmt.Println("Status:", res.Status, "User:", res.User)
+		}
+		close(wait)
+	}()
+
+	<-wait
 }
--- services/user.go
+++ services/user.go
@@ -92,3 +92,23 @@ func (*UserService) AddUsers(stream pb.UserService_AddUsersServer) error {
 		fmt.Println("Adding", req.GetName())
 	}
 }
+
+func (*UserService) AddUserStreamBoth(stream pb.UserService_AddUserStreamBothServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return nil
+		}
+		if err != nil {
+			log.Fatalf("Could not read stream: %v", err)
+		}
+
+		err = stream.Send(&pb.UserResultStream{
+			Status: "Added",
+			User:   req,
+		})
+		if err != nil {
+			log.Fatalf("Could not send stream: %v", err)
+		}
+	}
+}
